15681d99991e020b21afadd49c8d91100b5e8e9b
ashikulashi <ashismart554@gmail.com>
2022-02-25 12:49:47 +0530

    Done by felix

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,11 @@ package org.sample;
 
 public class Sample {
 	
+	private void add2() {
+		System.out.println("add2");
+
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Done");
